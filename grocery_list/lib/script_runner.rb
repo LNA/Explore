84d8c3beb96154b3df9170918f601d39f40e61f6
LNA <LaToyaNAllen@gmail.com>
2014-03-26 17:54:15 -0500

    refactors method to print contents

--- grocery_list/lib/script_runner.rb
+++ grocery_list/lib/script_runner.rb
@@ -8,9 +8,15 @@ class ScriptRunner
     parser.parse_contents
     parser.list.keys.each do |catagory|
       puts "We need to stock up on the following #{catagory}s:"
-      parser.list[catagory].each do |item|
-        puts "#{item}"
-      end
+      puts_items(parser.list, catagory)
+    end
+  end
+
+  private
+
+  def puts_items(list, catagory)
+    list[catagory].each do |item|
+      puts "#{item}" 
     end
   end
 end
