306fbfaec2385f935a9c38c42dacec72260fa4c5
LNA <LAllen@8thLight.com>
2014-04-30 11:12:58 -0500

    Changes variables to local

--- code_club/beginner/calculator/lib/calculator.rb
+++ code_club/beginner/calculator/lib/calculator.rb
@@ -1,22 +1,17 @@
 class Calculator
-  def initialize(first_number, second_number)
-    @first_number =  first_number
-    @second_number = second_number 
+  def add(first_number, second_number)
+    first_number + second_number 
   end
 
-  def add
-    @first_number + @second_number 
+  def subtract(first_number, second_number)
+    first_number - second_number 
   end
 
-  def subtract
-    @first_number - @second_number 
+  def multiply(first_number, second_number)
+    first_number * second_number 
   end
 
-  def multiply
-    @first_number * @second_number 
-  end
-
-  def divide
-    @first_number / @second_number 
+  def divide(first_number, second_number)
+    first_number / second_number 
   end
 end
--- code_club/beginner/calculator/spec/calculator_spec.rb
+++ code_club/beginner/calculator/spec/calculator_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
  
 describe Calculator do
-  let (:test_calculator) {Calculator.new(5,5)}
+  let (:test_calculator) {Calculator.new}
 
   it "adds two numbers together" do 
-    test_calculator.add.should == 10
+    test_calculator.add(5,5).should == 10
   end
 
   it "subtracts the second number from the first" do 
-    test_calculator.subtract.should == 0 
+    test_calculator.subtract(5,5).should == 0 
   end
 
   it "multiplies two numbers together" do 
-    test_calculator.multiply.should == 25 
+    test_calculator.multiply(5,5).should == 25 
   end
 
   it "divides the first number by the second number" do 
-    test_calculator.divide.should == 1
+    test_calculator.divide(5,5).should == 1
   end
 end
