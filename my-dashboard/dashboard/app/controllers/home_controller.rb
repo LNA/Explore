647d4dce7e5651100191f9e6b3953b7479ff914a
LNA <LAllen@8thLight.com>
2014-07-17 14:21:06 -0500

    Clojure Koans

--- my-dashboard/dashboard/app/controllers/home_controller.rb
+++ my-dashboard/dashboard/app/controllers/home_controller.rb
@@ -5,7 +5,8 @@ class HomeController < ApplicationController
     word_wrapper   = WordWrapper.new(dashboard.word_generator)
     day_wrapper    = DayWrapper.new(dashboard.day_finder)
 
-    @today         = day_wrapper.today 
+    @today         = day_wrapper.today #move to presenter so that I have one instance variable instead of 5
+    # @today       = @dashboard_presenter.today
     @am_task       = task_wrapper.build_am_tasks(@today)
     @pm_task       = task_wrapper.build_pm_tasks(@today)
     @word          = word_wrapper.word
