edeac44db7b2c10ffa1b3e8a9a09855897d7fcf6
LNA <LAllen@8thLight.com>
2014-10-26 20:06:58 -0500

    Parses xml file

--- record_parser/lib/parser.rb
+++ record_parser/lib/parser.rb
@@ -1,6 +1,33 @@
-class TextParser
-  def parse(input)
-    file = File.readlines(input)
+class Parser
+  def parse_text(file)
+    file = file_to_array(file)
     file.map {|record| record.split(' ')}
+  end
+
+  def parse_xml(file)
+    file = file_to_string(file)
+    records = []
+
+    parse_records(file).each do |record|
+      records << get_record(record)
+    end
+    records
+  end
+
+private
+  def file_to_array(file)
+    File.readlines(file)
+  end
+
+  def file_to_string(file)
+    File.read(file)
+  end
+
+  def get_record(record)
+    record[0].scan(/<\w+>(\w+\.?\d*)/).flatten
+  end
+
+  def parse_records(file_to_s)
+    file_to_s.scan(/<record>(.*?)<\/record>/m)
   end
 end
--- record_parser/spec/parser_spec.rb
+++ record_parser/spec/parser_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
  
-describe TextParser do
-  it "parses data" do
-    test_parser = Parser.new
-    parsed_records = test_parser.parse('data/input.txt')
+describe Parser do
+  let (:test_parser) { Parser.new }
 
-    expect(parsed_records.first).to eq ["Adams", "Wednesday", "F", "Black", "06/04/1989"]
+  it "parses a text file" do
+    parsed_text = test_parser.parse_text('data/input.txt')
+
+    expect(parsed_text.first).to eq ["Adams", "Wednesday", "F", "Black", "06041989"]
+    expect(parsed_text.last).to  eq ["Simone", "Nina", "F", "Green", "2211933"]
+  end
+
+  it "parses an xml file" do 
+    parsed_xml = test_parser.parse_xml('data/input.xml')
+
+    expect(parsed_xml.first).to eq ["Adams", "Wednesday", "F", "Black", "06041989"]
+    expect(parsed_xml.last).to  eq ["Simone", "Nina", "F", "Green", "2211933"]
   end
 end
